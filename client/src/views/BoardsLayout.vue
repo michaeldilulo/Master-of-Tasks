c01861136935c7cd675aa9cd5533804e3b32af95
michael dilulo <snoleopard01001101@gmail.com>
2020-11-09 12:33:18 -0800

    Application cleanup for fresh start

---
+++ client/src/views/BoardsLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="boards-layout"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "boardsLayout",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
